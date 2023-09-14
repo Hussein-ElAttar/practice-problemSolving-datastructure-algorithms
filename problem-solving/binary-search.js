6132e5e8e615003af52296d32c266cb0123d2e20
Hussein-ElAttar <hussien.elattar1@gmail.com>
2023-09-14 19:40:56 +0200

    Add recursive binary search

--- problem-solving/binary-search.js
+++ problem-solving/binary-search.js
@@ -20,7 +20,22 @@ const binarySearch = function (nums, target) {
     return -1;
 }
 
-const result = binarySearch([1, 2, 3, 4, 5, 6, 7], 3);
+function binarySearchRecursive(ar, target, left = 0, right = ar.length - 1) {
+    const middle = Math.ceil((right + left) / 2);
+
+    if (ar[middle] == target) {
+        return middle
+    }
+
+    if (ar[middle] > target) {
+        return binarySearch(ar, target, left, middle - 1);
+    }
+
+    return binarySearch(ar, target, middle + 1, right);
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
 console.log('=====================');
-console.log(result);
+console.log(binarySearch(arr, 3));
+console.log(binarySearchRecursive(arr, 3));
 console.log('=====================');

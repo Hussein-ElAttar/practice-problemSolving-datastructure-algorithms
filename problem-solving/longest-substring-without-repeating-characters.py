8840231b0fc42a3472407b7ae62ce9c7b504a17c
Hussein-ElAttar <hussien.elattar1@gmail.com>
2023-10-10 23:13:17 +0200

    Update variable names

--- problem-solving/longest-substring-without-repeating-characters.py
+++ problem-solving/longest-substring-without-repeating-characters.py
@@ -1,14 +1,12 @@
 def lengthOfLongestSubstring(self, s: str) -> int:
-    left = right = max_length = 0
-    substring_set = set()
+    left = right = longest = 0
+    seen = set()
     for right in range(len(s)):
-        if s[right] in substring_set:
-            while s[right] in substring_set:
-                substring_set.remove(s[left])
-                left += 1
+        while s[right] in seen:
+            seen.remove(s[left])
+            left += 1
 
-        substring_set.add(s[right])
-        right += 1
-        max_length = max(max_length, len(substring_set))
+        seen.add(s[right])
+        longest = max(longest, len(seen))
 
-    return max_length
+    return longest
